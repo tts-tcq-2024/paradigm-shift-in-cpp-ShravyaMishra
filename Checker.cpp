806f1ec787a2e0b2101062e901973031c36ace6e
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-07-08 12:35:31 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,53 @@
-#include <assert.h>
 #include <iostream>
-using namespace std;
+#include <unordered_map>
+#include <string>
+#include <cassert>
+
+struct MeasureLimits {
+    float lowerLimit;
+    float upperLimit;
+};
+
+std::unordered_map<std::string, MeasureLimits> measureLimits = {
+    {"Temperature", {0, 45}},
+    {"State of Charge", {20, 80}},
+    {"Charge Rate", {0, 0.8f}}
+};
+
+bool batteryIsOk(float temperature, float soc, float chargeRate);
+
+std::unordered_map<std::string, bool> checkMeasure(const std::string& measureName, float measureValue) {
+    std::unordered_map<std::string, bool> results;
+    bool status = true;
+    float lowerLimit = measureLimits[measureName].lowerLimit;
+    float upperLimit = measureLimits[measureName].upperLimit;
+
+    if (measureValue < lowerLimit) {
+        std::cout << measureName << " is too low!" << std::endl;
+        status = false;
+    } else if (measureValue > upperLimit) {
+        std::cout << measureName << " is too high!" << std::endl;
+        status = false;
+    }
+
+    results["status"] = status;
+    return results;
+}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+    auto tempResults = checkMeasure("Temperature", temperature);
+    auto socResults = checkMeasure("State of Charge", soc);
+    auto chargeResults = checkMeasure("Charge Rate", chargeRate);
+    return tempResults["status"] && socResults["status"] && chargeResults["status"];
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    // Test cases
+    assert(batteryIsOk(25, 70, 0.7f) == true);
+    assert(batteryIsOk(50, 85, 0.0f) == false);
+    assert(batteryIsOk(-1, 70, 0.7f) == false);
+    assert(batteryIsOk(25, 10, 0.7f) == false);
+    assert(batteryIsOk(25, 70, 0.9f) == false);
+    std::cout << "Some more tests needed" << std::endl;
+    return 0;
 }
